d522079bc3f38b068c0a9028623e9da79f6a299b
Ezequias <ezequias.net@gmail.com>
2021-03-14 20:07:59 -0300

    Center header tabs

--- frontend/components/TheHeader.vue
+++ frontend/components/TheHeader.vue
@@ -1,19 +1,18 @@
 <template>
   <header class="bg-neutral-800 mb-6">
     <div class="container">
-      <div class="flex items-center justify-between h-16">
+      <div class="flex items-center justify-center h-16">
         <nav class="flex items-baseline space-x-4">
           <NuxtLink :to="`/books`"> Books </NuxtLink>
           <NuxtLink :to="`/authors`"> Authors </NuxtLink>
         </nav>
-        <div class="flex-shrink-0 text-3xl">📚</div>
       </div>
     </div>
   </header>
 </template>
 <style scoped>
 nav a {
-  @apply px-3 py-2 rounded-md text-sm font-medium;
+  @apply px-3 py-2 rounded-full text-sm font-medium;
   @apply text-neutral-300 hover:bg-neutral-700 hover:text-white;
 }
 nav a.nuxt-link-active {
